7e6ad1f7dd3c4b9425780680920d29749c4f53af
n1ckdevops <nickdevops6@gmail.com>
2025-04-15 13:51:20 +0300

    add: css modules

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.scss'
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <a href="/" className={styles.link}>ToDo</a>
+      </div>
+    </header>
+  )
+}
--- src/pages/ToDoList.tsx
+++
@@ -1,5 +0,0 @@
-export const ToDoList = () => {
-  return (
-    <span>To-Do List Page</span>
-  )
-}
