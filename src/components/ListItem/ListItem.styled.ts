06cc9aae027eb885b285e81027b40f413f3ec96c
n1ckdevops <nickdevops6@gmail.com>
2025-07-11 01:13:56 +0300

    style(fix): bug fixes

---
+++ src/components/ListItem/ListItem.styled.ts
@@ -0,0 +1,10 @@
+/** @format */
+
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const StyledLink = styled(Link)<{ isDone: boolean }>`
+  text-decoration: none;
+  padding: 10px;
+  background-color: ${({ isDone }) => (isDone ? "lightgreen" : "lightcoral")};
+`;
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router-dom"
 import { ToDo } from "../../models/todo-item"
-import styles from './ListItem.module.scss'
+import { StyledLink } from "./ListItem.styled"
+
+
 export const ListItem = ({ todo }: { todo: ToDo }) => {
   return (
-    <Link className={`${styles.link} ${todo.isDone ? styles.done : styles.notDone}`}
-      to={`/list/${todo.id}`}>{todo.text}</Link>
+    <StyledLink isDone={todo.isDone} to={`/list/${todo.id}`}>
+      {todo.text}
+    </StyledLink>
   )
 }
